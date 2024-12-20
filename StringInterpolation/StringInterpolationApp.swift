1c6123dfcc8ed742c44758a08c7b984bcf6c886f
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-20 18:08:10 +0200

    Initial Commit

---
+++ StringInterpolation/StringInterpolationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringInterpolationApp.swift
+//  StringInterpolation
+//
+//  Created by Manyuchi, Carrington C on 2024/12/20.
+//
+
+import SwiftUI
+
+@main
+struct StringInterpolationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
